bdf941ad488e4c9941a05d985999a0bb708003d5
DotStarProgramming <office@dotstar.ca>
2020-09-08 18:49:16 -0400

    added a map

--- src/components/Child/Child.js
+++ src/components/Child/Child.js
@@ -1,11 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 
 import './Child.css';
 
 export default class Child extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			word: "waiting"
+		}
+	}
+
+	buttonIsClicked = () => {
+		this.setState({
+			word: "You clicked it!"
+		})
+	}
+
+	componentDidMount = () => {
+		this.setState({
+			word: "Mounted!"
+		})
+	}
+
 	render() {
 		return (
-			<span> {this.props.children} </span>
+			<>{this.props.children}</>
 		)
 	}
 }
--- src/components/Parent/Parent.js
+++ src/components/Parent/Parent.js
@@ -1,13 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './Parent.css';
-import Child from '../Child';
+import GoogleMapReact from 'google-map-react';
 
 export default class Parent extends Component {
+
+	static defaultProps = {
+		center: {
+			lat: 59.95,
+			lng: 30.33
+		},
+		zoom: 11
+	};
+
 	render() {
 		return (
-			<div className = "box">
-				<Child>hello world</Child>
+			<div style={{ height: '50vh', width: '50%' }}>
+				<GoogleMapReact
+					defaultCenter={this.props.center}
+					defaultZoom={this.props.zoom}
+				>
+				</GoogleMapReact>
 			</div>
 		)
 	}
